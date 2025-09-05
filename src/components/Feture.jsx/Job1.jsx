14f4879870b5b06a90192086f97d4eefb98a7cc7
aktarul-dve <islammdaktarul345@gmail.com>
2025-09-05 23:44:16 +0600

    Updated Work_regulations component

--- src/components/Feture.jsx/Job1.jsx
+++ src/components/Feture.jsx/Job1.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import math from '../../assets/math.jpg';
+import spin from '../../assets/spin.jpg';
+import ads from '../../assets/ads.jpg';
 
 const Job1 = () => {
 
@@ -24,35 +27,23 @@ const Job1 = () => {
         
         {/* Card 1 */}
         <div onClick={mathpage} className="bg-white rounded-2xl shadow-md overflow-hidden w-72 hover:shadow-xl transition">
-          <img
-            src="https://via.placeholder.com/300x200"
-            alt="Rule 1"
-            className="w-full h-20 object-cover"
-          />
-          <div className="p-4 text-center">
-            <p className=" text-sm">অংক করে টাকা</p>
-          </div>
-        </div>
-
-        {/* Card 2 */}
-        <div className="bg-white rounded-2xl shadow-md overflow-hidden w-72 hover:shadow-xl transition">
-          <img
-            src="https://via.placeholder.com/300x200"
-            alt="Rule 2"
-            className="w-full h-20 object-cover"
-          />
+          <img src={spin} alt="Work Rule 1" className="w-20 h-20 mx-auto object-cover" /> 
           <div className="p-4 text-center">
             <p className=" text-sm">স্পিন করে টাকা</p>
           </div>
         </div>
 
+        {/* Card 2 */}
+        <div className="bg-white rounded-2xl shadow-md overflow-hidden w-72 hover:shadow-xl transition">
+          <img src={math} alt="Work Rule 1" className="w-20 h-20 mx-auto object-cover" /> 
+          <div className="p-4 text-center">
+            <p className=" text-sm">অংক করে টাকা</p>
+          </div>
+        </div>
+
         {/* Card 3 */}
         <div onClick={watchAds} className="bg-white rounded-2xl shadow-md overflow-hidden w-72 hover:shadow-xl transition">
-          <img
-            src="https://via.placeholder.com/300x200"
-            alt="Rule 3"
-            className="w-full h-20 object-cover"
-          />
+          <img src={ads} alt="Work Rule 1" className="w-20 h-20 mx-auto object-cover" /> 
           <div className="p-4 text-center">
             <p className=" text-sm">অ্যাড দেখে টাকা</p>
           </div>
