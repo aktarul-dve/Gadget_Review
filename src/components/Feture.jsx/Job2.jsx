5d8aca1e31f85c9332b3878facd289e2a8438113
aktarul-dve <islammdaktarul345@gmail.com>
2025-09-06 11:04:13 +0600

    updage

--- src/components/Feture.jsx/Job2.jsx
+++ src/components/Feture.jsx/Job2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import quiz from "../../assets/quiz.jpg";
 const Job2 = () => {
   return (
     <div className="w-full bg-gray-100 py-5 px-2">
@@ -8,11 +8,8 @@ const Job2 = () => {
         
         {/* Card 1 */}
         <div className="bg-white rounded-2xl shadow-md overflow-hidden w-72 hover:shadow-xl transition">
-          <img
-            src="https://via.placeholder.com/300x200"
-            alt="Rule 1"
-            className="w-full h-20 object-cover"
-          />
+          <img src={quiz} alt="Work Rule 1" className="w-20 h-20 mx-auto object-cover" /> 
+          
           <div className="p-4 text-center">
             <p className="text-sm">বাংলা কুইজ</p>
           </div>
@@ -20,11 +17,7 @@ const Job2 = () => {
 
         {/* Card 2 */}
         <div className="bg-white rounded-2xl shadow-md overflow-hidden w-72 hover:shadow-xl transition">
-          <img
-            src="https://via.placeholder.com/300x200"
-            alt="Rule 2"
-            className="w-full h-20 object-cover"
-          />
+           <img src={quiz} alt="Work Rule 1" className="w-20 h-20 mx-auto object-cover" /> 
           <div className="p-4 text-center">
             <p className=" text-sm">অংক কুইজ</p>
           </div>
@@ -32,11 +25,7 @@ const Job2 = () => {
 
         {/* Card 3 */}
         <div className="bg-white rounded-2xl shadow-md overflow-hidden w-72 hover:shadow-xl transition">
-          <img
-            src="https://via.placeholder.com/300x200"
-            alt="Rule 3"
-            className="w-full h-20 object-cover"
-          />
+           <img src={quiz} alt="Work Rule 1" className="w-20 h-20 mx-auto object-cover" /> 
           <div className="p-4 text-center">
             <p className=" text-sm">ইংলিশ কুইজ</p>
           </div>
