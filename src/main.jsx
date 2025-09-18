aa45d514c450f3cf36cea760aeeee0ec255afabc
aktarul-dve <islamaktarul345@gmail.com>
2025-09-18 10:56:00 +0600

    neddd

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.jsx"
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// ✅ Service worker register করা হলো
+serviceWorkerRegistration.register();
