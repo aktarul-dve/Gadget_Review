adc4b4e053095b4ec668c92b2c878278d680d363
aktarul-dve <islamaktarul345@gmail.com>
2025-09-23 12:05:07 +0600

    nemmm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Hero from '../components/Feture/Hero'
 import MarqueeText from '../components/Feture/MarqueeText '
 import PopularAritcle from '../components/Feture/PopularArticle'
 
@@ -9,7 +8,6 @@ import PopularAritcle from '../components/Feture/PopularArticle'
 const Home = () => {
   return (
     <div >
-      <Hero/>
       <MarqueeText/>
       <PopularAritcle/>
     </div>
