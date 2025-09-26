92fc27b4c6d65695d4e8cb88181e42590b00dfd1
aktarul-dve <islamaktarul345@gmail.com>
2025-09-26 12:09:38 +0600

    mdvf

--- src/components/Feture/PopularArticle.jsx
+++ src/components/Feture/PopularArticle.jsx
@@ -36,7 +36,7 @@ const PopularArticle = ({ onItemClick }) => {
 
           <div key={index} className="flex bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl transition duration-300">
             <img src={ads} alt="ads" className="w-32 h-32 object-cover" />
-            <div className="p-4">
+            <div className="p-1">
               <h2 className="text-[15px]  mb-2">{item.Title}</h2>
               <p className="text-sm text-gray-700">
                 {item.Description.substring(0, 80) + "..."}
