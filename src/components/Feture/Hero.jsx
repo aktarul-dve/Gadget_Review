874915befd330071002d8278b33bd3496074dd62
aktarul-dve <islamaktarul345@gmail.com>
2025-09-28 21:30:30 +0600

    qqq

--- src/components/Feture/Hero.jsx
+++ src/components/Feture/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="w-full h-screen bg-gradient-to-br from-rose-200 to-cyan-50 flex items-center justify-center">
+    <section className="w-full h-48 bg-gradient-to-br from-rose-200 to-cyan-50 flex items-center justify-center">
       <div className="text-center px-4">
         {/* Main Heading */}
         <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-gray-800 drop-shadow-lg mb-4">
